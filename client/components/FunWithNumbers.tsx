8faecfb913149cfc8a72644fab467cf316ab6151
Gabriela <gaby@perez.co.nz>
2024-05-07 11:05:42 +1200

    fun with numbers

--- client/components/FunWithNumbers.tsx
+++ client/components/FunWithNumbers.tsx
@@ -1,18 +1,55 @@
 import { useState } from 'react'
 
 function FunWithNumbers() {
-  // TODO: create state
+  const [currentNumber, setCurrentNumber] = useState(3)
+
+  const minusThree = () => {
+    const result = currentNumber - 3
+    setCurrentNumber(result)
+  }
+
+  const handleClick = (operator: string, int: number) => {
+    switch (operator) {
+      case 'x':
+        setCurrentNumber(currentNumber * int)
+        break
+      case '/':
+        setCurrentNumber(currentNumber / int)
+        break
+      case '+':
+        setCurrentNumber(currentNumber + int)
+        break
+    }
+  }
 
   return (
     <>
       <h2>Fun with Numbers</h2>
-      <p>{/*state goes in here*/}</p>
+      <p>{currentNumber}</p>
 
       {/* TODO: create onClick code to update the state for each button */}
-      <button onClick={() => {}}>{/* something */}</button>
-      <button onClick={() => {}}>{/* something */}</button>
-      <button onClick={() => {}}>{/* something */}</button>
-      <button onClick={() => {}}>{/* something */}</button>
+      <button onClick={() => setCurrentNumber((num) => (num *= 5))}>* 5</button>
+      <button
+        onClick={() => {
+          minusThree()
+        }}
+      >
+        - 3
+      </button>
+      <button
+        onClick={() => {
+          handleClick('/', 2)
+        }}
+      >
+        / 2
+      </button>
+      <button
+        onClick={() => {
+          handleClick('+', 10)
+        }}
+      >
+        + 10
+      </button>
     </>
   )
 }
