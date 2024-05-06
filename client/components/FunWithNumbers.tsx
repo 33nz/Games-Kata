aa9e8beb32f2a08bf68cf1a3036ec5093bd17dcd
Gabriela <gaby@perez.co.nz>
2024-05-07 11:07:27 +1200

    added comments

--- client/components/FunWithNumbers.tsx
+++ client/components/FunWithNumbers.tsx
@@ -3,11 +3,13 @@ import { useState } from 'react'
 function FunWithNumbers() {
   const [currentNumber, setCurrentNumber] = useState(3)
 
+  // Example of creating a seperate function for each 'onClick'
   const minusThree = () => {
     const result = currentNumber - 3
     setCurrentNumber(result)
   }
 
+  // Example of creating a single 'handleClick' function with conditionals 
   const handleClick = (operator: string, int: number) => {
     switch (operator) {
       case 'x':
@@ -27,7 +29,7 @@ function FunWithNumbers() {
       <h2>Fun with Numbers</h2>
       <p>{currentNumber}</p>
 
-      {/* TODO: create onClick code to update the state for each button */}
+      {/* Example of changing state directly inside the 'onClick' */}
       <button onClick={() => setCurrentNumber((num) => (num *= 5))}>* 5</button>
       <button
         onClick={() => {
