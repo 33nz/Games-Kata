a4f6b5cdbe8d2a72bfea8ed8d918c0e82a1b9cca
David Kavenga <david.kavenga@gmail.com>
2024-05-07 16:57:44 +1200

    shenanigan mischief managed

--- client/components/ListShenanigans.tsx
+++ client/components/ListShenanigans.tsx
@@ -19,6 +19,53 @@ function ListShenanigans() {
     ],
   })
 
+  // We can use any kind of state variable we like
+  // const [leftList, setLeftList] = useState([
+  //   'Apple',
+  //   'DVD Player',
+  //   'Mysterious Smell',
+  //   'Pompom Hat',
+  //   'Praying Mantis',
+  //   'Skeletor',
+  // ])
+  // const [rightList, setRightList] = useState([
+  //   'Caltrops',
+  //   'Chad Maywether Jr.',
+  //   'Comedy Bang! Bang!',
+  //   'Freshly Cut Grass',
+  //   'Stop Sign',
+  // ])
+
+  const handleClick = (item: string, direction: string) => {
+    // Two ways to set the state directly, or with a callback
+    setLists({ left: [...lists.left], right: [...lists.right] })
+    setLists((oldLists) => {
+      return { left: [...oldLists.left], right: [...oldLists.right] }
+    })
+
+    // move left to right with arrays
+    if (direction === 'right') {
+      // setLists((prevLists) => {
+      //   const newLeft = prevLists.left.filter((i) => i !== item)
+      //   const newRight = [...prevLists.right, item].sort()
+      //   return { left: newLeft, right: newRight }
+      // })
+      const leftCopy = [...lists.left]
+      const rightCopy = [...lists.right]
+      rightCopy.push(item)
+      leftCopy.splice(lists.left.indexOf(item), 1)
+      setLists({ left: leftCopy, right: rightCopy.sort() })
+    }
+    // move right to left (callback style)
+    else if (direction === 'left') {
+      setLists((prevLists) => {
+        const newRight = prevLists.right.filter((i) => i !== item)
+        const newLeft = [...prevLists.left, item].sort()
+        return { left: newLeft, right: newRight }
+      })
+    }
+  }
+
   return (
     <>
       <h2>List Shenanigans</h2>
@@ -27,12 +74,20 @@ function ListShenanigans() {
         <ul>
           {lists.left.map((item) => (
             <li key={item}>
-              {item} <button>→</button>
+              {item}{' '}
+              <button onClick={() => handleClick(item, 'right')}>→</button>
             </li>
           ))}
         </ul>
         {/* right list */}
-        {/* use the ← character for the button in this list */}
+        <ul>
+          {lists.right.map((item) => (
+            <li key={item}>
+              <button onClick={() => handleClick(item, 'left')}>←</button>{' '}
+              {item}
+            </li>
+          ))}
+        </ul>
         <ul></ul>
       </section>
     </>
