318b05d6cbbec719f007433c76cdf1908326f4b7
Gabriela <gaby@perez.co.nz>
2024-05-07 14:55:23 +1200

    finished pixel party hivemind"

--- client/components/Pixel.tsx
+++ client/components/Pixel.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 function Pixel() {
   const [color, setColor] = useState(getColor())
+  const [lock, setLock] = useState(false)
 
   function getColor() {
     return `#${Math.floor(Math.random() * 0x1000000)
@@ -9,7 +10,32 @@ function Pixel() {
       .padStart(6, '0')}`
   }
 
-  return <div className="pixel" style={{ background: color }}></div>
+  const handleClick = () => {
+    if (!lock) {
+      setColor(getColor())
+    }
+  }
+
+  const handleDoubleClick = () => {
+    setLock(true)
+    setColor('#000000')
+  }
+
+  const handleRightClick = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setLock(false)
+    setColor(getColor())
+  }
+
+  return (
+    <div
+      onContextMenu={handleRightClick}
+      onDoubleClick={handleDoubleClick}
+      onClick={handleClick}
+      className="pixel"
+      style={{ background: color }}
+    ></div>
+  )
 }
 
 export default Pixel
